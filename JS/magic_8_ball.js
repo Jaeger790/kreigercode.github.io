8485dce9007aa27a84f4f0c72f054709ab32fb23
Jaeger790 <b.rott8401@gmail.com>
2021-11-19 08:19:31 -0800

    eightball

---
+++ JS/magic_8_ball.js
@@ -0,0 +1,42 @@
+let userName = 'Brad'
+
+userName ? console.log('Hello ' + userName + "!") 
+    : console.log('Hello!');
+
+let userQuestion = 'Will you leave your fate up to the magic 8 ball?';
+
+    console.log(userName + ' ' + userQuestion);
+    
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+    
+switch(randomNumber){
+    case 0:
+        eightBall = 'It is certain'
+            break;
+    case 1:
+        eightBall = 'It is decidedly so'
+            break;
+    case 2:
+        eightBall = 'reply hazy try again'
+            break;
+    case 3:
+        eightBall = 'Cannot predict now'
+            break;
+    case 4:
+        eightBall = 'Do not count on it'
+            break;
+    case 5:
+        eightBall = 'My sources say no'
+            break;
+    case 6:
+        eightBall = 'Outlook not so good'
+        break;
+    case 7:
+        eightBall = 'Signs point to yes'
+        break;
+
+}
+
+console.log(eightBall);
