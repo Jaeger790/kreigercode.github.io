ada80169a0676d8a13d12876e52bb10f909ce0e0
Bradley Rott <brott1@wgu.edu>
2022-07-27 19:20:16 -0700

    shopping cart portfolio item

---
+++ data.js
@@ -0,0 +1,25 @@
+let shopItemData = [{
+  id: "roseCard",
+  name: "Rose",
+  price: 7.50,
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+  img: "pics/Rose.jpg"
+},{
+  id: "dhaliaCard",
+  name: "Dhalia",
+  price: 10.00,
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+  img: "pics/Dhalia.jpg"
+},{
+  id: "tulipCard",
+  name: "Tulip",
+  price: 7.50 ,
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+  img: "pics/Tulip.jpg"
+},{
+  id: "flowerCard",
+  name: "Hygraenga",
+  price: 5.50,
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+  img: "pics/Hygraenga.jpg"
+}];
