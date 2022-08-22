2780c70c11eb37f25d06e50e309daf3e8eb9ea92
Bradley Rott <brott1@wgu.edu>
2022-08-21 18:19:37 -0700

    Add files via upload

---
+++ ComputerRepair/app.js
@@ -0,0 +1,77 @@
+
+
+
+let phoneValidateMessage = document.getElementById("phone")
+phoneValidateMessage.oninvalid = function(event){
+    event.target.setCustomValidity("Phone number should match format: 123-456-789")
+};
+
+/* 
+    ! Animation funtions
+*/
+
+
+
+function phoneFormat(obj){
+    obj.getElementById("")
+}
+
+
+/* 
+    ! Navbar funtions
+*/
+
+function coll(){
+  document.getElementById("navBar").style.display = "none";
+  document.getElementById("showNav").style.display = "block";
+ 
+  //console.log("hide")
+}
+
+function show(){
+    document.getElementById("navBar").style.display = "block";
+    document.getElementById("showNav").style.display = "none";
+    document.getElementById("collapseNavButton").style.float = "right";
+   //console.log("show")
+}
+
+
+/* 
+    ! Scroll funtions
+*/
+
+function scrollToTop(){
+    //document.querySelector("#top").scrollIntoView({behavior:"smooth"});
+    window.scrollTo({top:0, behavior: "smooth"});
+}
+
+function scrollBottom(){
+    document.querySelector("#contact").scrollIntoView({behavior:"smooth"});
+    
+}
+
+function scrollAbout(){
+    document.querySelector("#about").scrollIntoView({behavior:"smooth"});
+}
+
+function scrollService(){
+    document.querySelector("#services").scrollIntoView({behavior:"smooth"});
+}
+
+
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "b.rott8401@gmail.com",
+        Password : "Codefacers790500!",
+        To : 'b.rott8401@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New Appointment Request",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+
+    sendEmail()
+}
