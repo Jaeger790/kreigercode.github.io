063a09d06c7d1a9931a7eea4f2b79adeb757a0a4
Bradley Rott <brott1@wgu.edu>
2022-09-17 07:50:39 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const ctx = myShape.getContext("2d");
 
 
 ctx.fillStyle= '#6a859b';
-ctx.moveTo(0,10);
-ctx.lineTo(1920,10);
-ctx.lineTo(300,1250);
-ctx.lineTo(800,500);
+ctx.moveTo(5,10);
+ctx.lineTo(1800,10);
+ctx.lineTo(1350, 400);
+ctx.lineTo(1800,900);
 ctx.fill();
 
 
@@ -22,4 +22,18 @@ window.onscroll = function(){
         document.getElementById("navBar").style.top = "-100px";
     }
     hideScroll = scrollPos;
+}
+
+//scroll functions
+function scrollToTop(){
+    window.scrollTo({top:0, behavior:"smooth"})
+
+}
+
+function scrollAbout(){
+    document.querySelector('#about').scrollIntoView({behavior:"smooth"})
+}
+
+function scrollTemplate(){
+    document.querySelector('#template').scrollIntoView({behavior:"smooth"})
 }
