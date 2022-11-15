5e56ca0395ea56a9ccf22033bca1a1d164d3b272
Brad Rott <brott1@wgu.edu>
2022-11-15 12:46:56 -0800

    Add files via upload

--- BlogPage/blogApp.js
+++ BlogPage/blogApp.js
@@ -1,9 +1,41 @@
 function showTechBlog(){
-    document.getElementById('motoLog').style.display = 'none';
-    document.getElementById('techBlog').style.display = 'flex';
+    fadeOutMoto();
+    fadeIn();
+    //document.getElementById('motoLog').style.display = 'none';
 }
 
 function showMotoLog(){
-    document.getElementById('motoLog').style.display = 'flex';
+    fadeOut();
+    fadeInMoto();
+    //document.getElementById('techBlog').style.display= 'none';
+}
+function showAll(){
+    fadeIn();
+    fadeInMoto();
+}
+
+function fadeOut(){
+    document.getElementById('techBlog').style.transition = "all .5s";
+    document.getElementById('techBlog').style.transform = "scale(0)";
     document.getElementById('techBlog').style.display = 'none';
 }
+function fadeIn(){
+    
+    document.getElementById('techBlog').style.transition = "all .5s";
+    document.getElementById('techBlog').style.transform = "scale(1)";
+   document.getElementById('techBlog').style.display = 'flex';
+    
+}
+
+function fadeInMoto(){
+    document.getElementById('motoLog').style.transition = "all .5s";
+    document.getElementById('motoLog').style.transform = "scale(1)";
+   document.getElementById('motoLog').style.display = 'flex';
+}
+
+function fadeOutMoto(){
+    document.getElementById('motoLog').style.transition = "all .5s";
+    document.getElementById('motoLog').style.transform = "scale(0)";
+    document.getElementById('motoLog').style.display = 'none';
+}
+
