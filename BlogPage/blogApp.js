3fcbd6e709554678279de95de89429f598555416
Brad Rott <brott1@wgu.edu>
2022-11-15 10:28:26 -0800

    Add files via upload

---
+++ BlogPage/blogApp.js
@@ -0,0 +1,9 @@
+function showTechBlog(){
+    document.getElementById('motoLog').style.display = 'none';
+    document.getElementById('techBlog').style.display = 'flex';
+}
+
+function showMotoLog(){
+    document.getElementById('motoLog').style.display = 'flex';
+    document.getElementById('techBlog').style.display = 'none';
+}
