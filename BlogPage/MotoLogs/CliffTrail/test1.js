5c3739a7cb790889f8ac0bdb79325dab6bb7642e
Jaeger790 <b.rott8401@gmail.com>
2022-12-24 12:34:42 -0800

    updates

--- BlogPage/MotoLogs/CliffTrail/test1.js
+++ BlogPage/MotoLogs/CliffTrail/test1.js
@@ -2,84 +2,66 @@ const descriptionBubble1 = document.querySelector("#desc1");
 const descriptionBubble2 = document.querySelector("#desc2");
 const bubbleArrow = document.querySelector(".bubbleArrow");
 const bubbleArrow2 = document.querySelector(".bubbleArrow2");
-
-let bubbleHeight1 = descriptionBubble1.style.height = "10vh";
-let bubbleHeight2 = descriptionBubble2.style.height = "10vh";
-
-
-
+//initialize description bubble height
+let bubbleHeight1 = (descriptionBubble1.style.height = "20vh");
+let bubbleHeight2 = (descriptionBubble2.style.height = "20vh");
 
 //expand collapse description bubble 1
-descriptionBubble1.addEventListener("click",()=>{
-    console.log("Click");
-    
-    let bubbleShow1 = descriptionBubble1.style.height = "10vh";
+descriptionBubble1.addEventListener("click", () => {
+    let bubbleShow1 = (descriptionBubble1.style.height = "20vh");
 
-    if(bubbleHeight1 == bubbleShow1){
+    if (bubbleHeight1 == bubbleShow1) {
         bubbleArrow.style.transition = ".3s";
         bubbleArrow.style.transform = "rotate(180deg)";
         bubbleArrow.style.float = "right";
-        
-        let bubbleHide1 = descriptionBubble1.style.height = "auto";
-        bubbleHeight1 = bubbleHide1;
-        descriptionBubble1.scrollIntoView({behavior: "smooth"});
 
-    }else{
+        let bubbleHide1 = (descriptionBubble1.style.height = "auto");
+        bubbleHeight1 = bubbleHide1;
+        descriptionBubble1.scrollIntoView({ behavior: "smooth" });
+    } else {
         bubbleHeight1 = bubbleShow1;
         bubbleArrow.style.transform = "rotate(0deg)";
     }
 });
 
 // expand collapse description bubble 2
-descriptionBubble2.addEventListener("click",()=>{
-
-    let bubbleShow2 = descriptionBubble2.style.height = "10vh";
+descriptionBubble2.addEventListener("click", () => {
+    let bubbleShow2 = (descriptionBubble2.style.height = "20vh");
     bubbleArrow2.style.transition = ".3s";
     bubbleArrow2.style.transform = "rotate(180deg)";
     bubbleArrow2.style.float = "right";
-    if(bubbleHeight2 == bubbleShow2){
-        
-        let bubbleHide2 = descriptionBubble2.style.height = "auto";
+    if (bubbleHeight2 == bubbleShow2) {
+        let bubbleHide2 = (descriptionBubble2.style.height = "auto");
         bubbleHeight2 = bubbleHide2;
-        descriptionBubble2.scrollIntoView({behavior: "smooth"});
-    }else{
-       bubbleHeight2 = bubbleShow2;
-       bubbleArrow2.style.transform = "rotate(0deg)";
-     }
-     
- });
+        descriptionBubble2.scrollIntoView({ behavior: "smooth" });
+    } else {
+        bubbleHeight2 = bubbleShow2;
+        bubbleArrow2.style.transform = "rotate(0deg)";
+    }
+});
 
- //change background color of description bubble 1 when hovered over
- descriptionBubble1.addEventListener("mouseenter",()=>{
+//change background color of description bubble 1 when hovered over
+descriptionBubble1.addEventListener("mouseenter", () => {
     descriptionBubble1.style.transition = "1s";
     descriptionBubble1.style.background = "rgba(51, 61, 41,.4)";
     descriptionBubble1.style.boxShadow = "10px 20px 30px rgb(64, 67, 40)";
-    
-    
-    
- });
+});
 //change background color back to original color when mouse leaves div
- descriptionBubble1.addEventListener("mouseleave",()=>{
+descriptionBubble1.addEventListener("mouseleave", () => {
     descriptionBubble1.style.transition = "1s";
     descriptionBubble1.style.background = "rgba(194, 197, 170,0)";
     descriptionBubble1.style.boxShadow = "none";
-    
-    
- });
+});
 
- //change background color of description bubble 2 when hovered over
- descriptionBubble2.addEventListener("mouseenter",()=>{
+//change background color of description bubble 2 when hovered over
+descriptionBubble2.addEventListener("mouseenter", () => {
     descriptionBubble2.style.transition = "1s";
     descriptionBubble2.style.background = "rgba(51, 61, 41,.4)";
     descriptionBubble2.style.boxShadow = "10px 20px 30px rgb(64, 67, 40)";
-    
-    
-    
- });
- //change background color back to original color when mouse leaves div
- descriptionBubble2.addEventListener("mouseleave",()=>{
+});
+//change background color back to original color when mouse leaves div
+descriptionBubble2.addEventListener("mouseleave", () => {
     descriptionBubble2.style.transition = "1s";
     descriptionBubble2.style.background = "rgba(194, 197, 170,0)";
     descriptionBubble2.style.boxShadow = "none";
-    
- });
+});
--- BlogPage/blogApp.js
+++ BlogPage/blogApp.js
@@ -1,62 +1,66 @@
-let techBlog = document.getElementById('techBlog');
-let motoLog = document.getElementById("motoLog");
-
-
-
-function showTechBlog(){
+//blogs by content type
+let techBlog = document.getElementsByClassName("tList");
+let motoLog = document.getElementsByClassName("mList");
+//arrays for blog elements
+let techBlogElements = Array.from(techBlog);
+let motoBlogElements = Array.from(motoLog);
+//top nav-bar buttons
+let showMotoB = document.getElementById("showMoto");
+let showTechB = document.getElementById("showTech");
+let showAllB = document.getElementById("showAll");
+//hide moto blog items and show tech blog items
+function showTechBlog() {
     fadeOutMoto();
-    setTimeout(()=>fadeIn(), 400);
-    
+    setTimeout(() => fadeInTech(), 400);
 }
-
-function showMotoLog(){
-    fadeOut();
-    setTimeout(()=>fadeInMoto(), 400);
-    
+//show moto blog items and hide tech blog items
+function showMoto() {
+    fadeOutTech();
+    setTimeout(() => fadeInMoto(), 400);
 }
-function showAll(){
-    fadeIn();
+//show all blog items
+function showAll() {
+    fadeInTech();
     fadeInMoto();
 }
-
-function fadeOut(){
-    techBlog.style.transition = "all .2s";
-    
-    techBlog.style.transform = "scale(0)";
-    setTimeout(()=>techBlog.style.display = 'none', 200);
-}
-function fadeIn(){
-    
-    if(techBlog.style.display = "none"){
-        techBlog.style.display = 'flex';
-        
-        techBlog.style.transition = "all .2s";
-        setTimeout(()=>techBlog.style.transform = "scale(1)", 1);
-        
-        
+/* iterate through an array of motoBlog div elements and add animation to hide them. */
+function fadeOutMoto() {
+    for (let i = 0; i < motoBlogElements.length; i++) {
+        motoLog[i].style.transition = "all .2s";
+        motoLog[i].style.transform = "scale(0)";
+        setTimeout(() => (motoLog[i].style.display = "none"), 200);
     }
-
-    
-    
-    
 }
-
-function fadeInMoto(){
-
-    if(motoLog.style.display = "none"){
-        motoLog.style.display = 'flex';
-        motoLog.style.transition = "all .2s";
-        setTimeout(()=>motoLog.style.transform = "scale(1)", 1);
+/* iterate through an array of motoBlog div elements and add animation to fade them into view. */
+function fadeInMoto() {
+    for (let i = 0; i < motoBlogElements.length; i++) {
+        if ((motoLog[i].style.display = "none")) {
+            motoLog[i].style.display = "flex";
+            motoLog[i].style.transition = "all .2s";
+            setTimeout(() => (motoLog[i].style.transform = "scale(1)"), 1);
+        }
     }
-        
-    
 }
-
-function fadeOutMoto(){
-    motoLog.style.transition = "all .2s";
-    motoLog.style.transform = "scale(0)";
-    setTimeout(()=> motoLog.style.display = 'none', 200);
+/* iterate through an array of techBlog div elements and add animation to fade them out of view. */
+function fadeOutTech() {
+    for (let i = 0; i < techBlogElements.length; i++) {
+        techBlog[i].style.transition = "all .2s";
+        techBlog[i].style.transform = "scale(0)";
+        setTimeout(() => (techBlog[i].style.display = "none"), 200);
+    }
 }
+/* iterate through an array of techBlog div elements and add animation to fade them in view. */
+function fadeInTech() {
+    for (let i = 0; i < techBlogElements.length; i++) {
+        if ((techBlog[i].style.display = "none")) {
+            techBlog[i].style.display = "flex";
 
-
-
+            techBlog[i].style.transition = "all .2s";
+            setTimeout(() => (techBlog[i].style.transform = "scale(1)"), 1);
+        }
+    }
+}
+//add event listeners on the top nav buttons to call blog filtering methods
+showMotoB.addEventListener("click", showMoto);
+showTechB.addEventListener("click", showTechBlog);
+showAllB.addEventListener("click", showAll);
