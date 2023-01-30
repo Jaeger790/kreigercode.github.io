9478b640b9ced6f7551e3c8bfb19c375d5fa75b1
Jaeger790 <b.rott8401@gmail.com>
2023-01-29 20:41:50 -0800

    changed language display on home page and added animation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const navBar = document.getElementById("navBar");
 const downloadIcon = document.getElementById("downloadIcon");
 const resume = document.getElementById("resumeLink");
 
+
 resume.style.borderRadius = "15px";
 
 
@@ -47,4 +48,81 @@ function scrollAbout(){
 function scrollTemplate(){
     document.querySelector('#template').scrollIntoView({behavior:"smooth"})
 }
+
+
+
+
+/*
+    Experience bar functions for programming languages
+*/
+
+const innerLevelBars = document.getElementsByClassName("langBarInner");
+const javaInnerBar = document.getElementById("javaInnerBar");
+let javaLevelText = document.getElementById("javaLevelValue");
+
+const cSharpInnerBar = document.getElementById("cSharpInnerBar");
+const cPlusInnerBar = document.getElementById("cPlusInnerBar");
+const jsInnerBar = document.getElementById("jsInnerBar");
+const pythonInnerBar = document.getElementById("pythonInnerBar");
+const skillSection = document.getElementById("skillSection");
+const langLevelContainer = document.getElementsByClassName("langLevelContainer");
+const skillS = document.querySelector("#skillSection");
+const langHead = document.getElementById("langHead");
+let position = 0;
+
+javaInnerBar.style.left = "0";
+langHead.style.opacity = "0";
+
+
+function animateFadeIn(){
+    langHead.style.transition = " 2s";
+    setTimeout(()=>(langHead.style.opacity ++),300);
+    langHead.style.transform = 'translateX(5vw)';
+
+    for (let i = 0; i < langLevelContainer.length; i++) {
+        langLevelContainer[i].style.transition = " 2s";
+        setTimeout(()=>(langLevelContainer[i].style.opacity ++),600);
+        langLevelContainer[i].style.transform = "translateX(10vw)";    
+    }
+    
+    for(i=0;i<innerLevelBars.length;i++){
+        innerLevelBars[i].style.transition = "all 3s";
+        setTimeout(()=> (javaInnerBar.style.width = "95%",200));
+        setTimeout(()=> (cSharpInnerBar.style.width = "80%",200));
+        setTimeout(()=> (cPlusInnerBar.style.width = "70%",200));
+        setTimeout(()=> (jsInnerBar.style.width = "77%",200));
+        setTimeout(()=> (pythonInnerBar.style.width = "84%",200));
+    }
+
+    
+
+}
+
+
+
+function animateFadeOut(){
+    for (let i = 0; i < langLevelContainer.length; i++) {
+        langLevelContainer[i].style.transition = " 2s";
+        langLevelContainer[i].style.opacity --;
+        langLevelContainer[i].style.transform = "translateX(-150px)";
+    }
+    langHead.style.transition = " 2s";
+    setTimeout(()=>(langHead.style.opacity --));
+    langHead.style.transform = 'translateX(-10vw)';
+}
+
+
+
+
+
+
+var observer = new IntersectionObserver(function(entries) {
+	if(entries[0].isIntersecting === true){
+        animateFadeIn();
+    } else{
+        animateFadeOut();    
+    }    
+}, { threshold: [0] });
+
+observer.observe(skillS);
 
